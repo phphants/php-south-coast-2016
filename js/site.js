ef4bb5a01ae982a912d031ad6c75143a9e836599
Lee Boynton <lee@lboynton.com>
2015-11-29 17:26:49 +0000

    Remove unused JS/CSS

--- js/site.js
+++ js/site.js
@@ -1,14 +1,3 @@
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 $('div.modal').on('show.bs.modal', function() {
 	var modal = this;
 	var hash = modal.id;
