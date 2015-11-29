cc1c2e035580f46dcbc8138b0331ce54a4e2e75a
Lee Boynton <lee@lboynton.com>
2015-11-29 16:17:17 +0000

    Add home page and code of conduct content

--- js/site.js
+++ js/site.js
@@ -9,16 +9,6 @@ $(function() {
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 $('div.modal').on('show.bs.modal', function() {
 	var modal = this;
 	var hash = modal.id;
